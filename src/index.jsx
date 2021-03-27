a0bb7e027e5d3c1dc627299c19cdeb02e9e8e857
mbp1980 <73567442+mbp1980@users.noreply.github.com>
2021-03-27 15:19:54 -0700

    added import from MainView
    cleaned up spacing

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { MainView } from "./components/main-view/main-view"
+
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
@@ -8,7 +10,8 @@ import "./index.scss";
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-
+    return <MainView/>
+  
   }
 }
 
